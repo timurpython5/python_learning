55a3d9785a0ca8a355dc13624d7870bf8243312a
guop8 <guop85@gmail.com>
2024-02-13 21:06:11 +0300

    metanit.basics

---
+++ peremennie/__init__.py
@@ -0,0 +1,44 @@
+'''
+Timur = False
+print(Timur)
+
+Artur = True
+print(Artur) '''
+
+"""
+age = 21
+print("Возраст:", age)    # Возраст: 21
+
+count = 15
+print("Количество:", count) # Количество: 15 """
+
+"""
+i = input("Введите свое имя: ")
+print("Здравствуйте,", i) """
+
+"""
+height = 1.68
+pi = 3.14
+weight = 68.
+print(height)
+print(pi)
+print(weight)"""
+
+"""
+text = ("Laudate omnes gentes laudate "
+        "Magnificat in secula ")
+print(text) """
+
+'''
+Это комментарий
+'''
+text = '''Laudate omnes gentes laudate
+Magnificat in secula
+Et anima mea laudate
+Magnificat in secula 
+'''
+"""
+name = str(input("Введите имя: "))
+age = int(input("Введите возраст: "))
+favouriteGame = str(input("Введите любимую игру: "))
+print(f"Привет, я {name}, мне {age}, я люблю играть в игру {favouriteGame}") """
